5825136ae8b7a648e1e2205174960ad15717cc61
Andrii Pasichnik <67899666+pasichDev@users.noreply.github.com>
2024-01-24 22:15:05 +0200

    Delete ui directory

--- ui/theme/style/dialog_style.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class DialogStyle {
-  static const borderShape = RoundedRectangleBorder(
-  borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
-  );
-}
