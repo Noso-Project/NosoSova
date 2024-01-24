a3d1bda82b663b2585570f414fac63a4f8254076
Andrii Pasichnik <67899666+pasichDev@users.noreply.github.com>
2024-01-24 22:14:58 +0200

    Delete utils directory

--- utils/date_utils.dart
+++
@@ -1,8 +0,0 @@
-import 'package:intl/intl.dart';
-
-class DateUtil{
-  static String getUtcTime(int unixTime) {
-    DateTime dateTime = DateTime.fromMillisecondsSinceEpoch(unixTime * 1000, isUtc: true);
-    return DateFormat('HH:mm:ss').format(dateTime);
-  }
-}
--- utils/other_utils.dart
+++
@@ -1,8 +0,0 @@
-class OtherUtils {
-  static String hashObfuscation(String hash) {
-    if (hash.length >= 25) {
-      return "${hash.substring(0, 9)}...${hash.substring(hash.length - 9)}";
-    }
-    return hash;
-  }
-}
