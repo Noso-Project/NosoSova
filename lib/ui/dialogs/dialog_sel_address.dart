088582c53715e31ee9f65adce76d323efe1f49b0
pasichDev <apasichnik9@gmail.com>
2024-03-28 21:04:43 +0200

    fix: address list

--- lib/ui/dialogs/dialog_sel_address.dart
+++ lib/ui/dialogs/dialog_sel_address.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nososova/blocs/wallet_bloc.dart';
 
+import '../../l10n/app_localizations.dart';
 import '../../models/address_wallet.dart';
+import '../common/widgets/empty_list_widget.dart';
 import '../tiles/tile_wallet_address.dart';
 
 class DialogSellAddress extends StatefulWidget {
@@ -25,34 +27,39 @@ class DialogSellAddressState extends State<DialogSellAddress> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const SizedBox(height: 0),
-          Container(
-            padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-            width: double.infinity,
-            height: MediaQuery.of(context).size.height * 0.4,
-            child: ListView.builder(
-              shrinkWrap: true,
-              //   controller: _controller,
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-              itemCount: listAddress.length,
-              itemBuilder: (context, index) {
-                final item = listAddress[index];
-                return Container(
-                    color: widget.targetAddress.hash == item.hash
-                        ? Colors.grey.withOpacity(0.2)
-                        : Colors.transparent,
-                    child: AddressListTile(
-                      address: item,
-                      onLong: () {},
-                      onTap: () {
-                        Navigator.pop(context);
-                        widget.selected(item);
-                      },
-                    ));
-              },
-            ),
-          )
+          if (listAddress.isEmpty)
+            Padding(
+                padding: const EdgeInsets.symmetric(vertical: 70),
+                child: EmptyWidget(
+                    title: AppLocalizations.of(context)!.empty,
+                    descrpt: AppLocalizations.of(context)!.emptyListAddress)),
+          if (listAddress.isNotEmpty)
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.4,
+              child: ListView.builder(
+                shrinkWrap: true,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
+                itemCount: listAddress.length,
+                itemBuilder: (context, index) {
+                  final item = listAddress[index];
+                  return Container(
+                      color: widget.targetAddress.hash == item.hash
+                          ? Colors.grey.withOpacity(0.2)
+                          : Colors.transparent,
+                      child: AddressListTile(
+                        address: item,
+                        onLong: () {},
+                        onTap: () {
+                          Navigator.pop(context);
+                          widget.selected(item);
+                        },
+                      ));
+                },
+              ),
+            )
         ],
       );
     });
