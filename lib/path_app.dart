33b2d4c17b24321b267763cd1fc81b4b7933495f
pasichDev <apasichnik9@gmail.com>
2024-05-18 01:48:54 +0300

    fix linux run

--- lib/path_app.dart
+++ lib/path_app.dart
@@ -4,7 +4,6 @@ import 'package:path_provider/path_provider.dart';
 
 class PathAppUtil {
   static getAppPath() async {
-    print(await getLibraryDirectory());
     if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
       var path = Platform.isMacOS
           ? await getLibraryDirectory()
